3fa0d731c9b1b7284eb62bcbe1366650f0705346
ItokianaIZ07 <itokianarabarivelo@gmail.com>
2026-06-09 15:24:51 +0300

    creation servlet

--- src/main/java/com/mnemos/servlet/FrontControllerServlet.java
+++ src/main/java/com/mnemos/servlet/FrontControllerServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class FrontServlet extends HttpServlet {
+public class FrontControllerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String URL = req.getContextPath();

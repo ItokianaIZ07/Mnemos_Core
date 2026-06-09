b1de6108f925b11e75d0ae9bf10cc46b0cbc1de4
ItokianaIZ07 <itokianarabarivelo@gmail.com>
2026-06-09 15:18:48 +0300

    init commit

---
+++ src/main/java/com/mnemos/servlet/FrontServlet.java
@@ -0,0 +1,28 @@
+package com.mnemos.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FrontServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String URL = req.getContextPath();
+        processRequest(resp, URL);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String URL = req.getContextPath();
+        processRequest(resp, URL);
+    }
+
+    private void processRequest(HttpServletResponse res, String url) throws IOException {
+        PrintWriter out = res.getWriter();
+        out.println(url);
+    }
+}
---
+++ todo.md
@@ -0,0 +1,10 @@
+## Instruction Sprint 0:
+- Création d'un servlet: FrontControllerServlet
+- doGet
+- doPost
+- processRequest(appelé par doGet et doPost)
+
+**Objectif:**
+- Afficher l'url par le processRequest
+- Pour le test:
+* configurer le web.xml du test afin d'initialiser le servlet et aussi les request param

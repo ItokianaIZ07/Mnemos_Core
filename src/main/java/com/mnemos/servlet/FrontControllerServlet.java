43804fcd045364437aa986c0f9808301748814b6
ItokianaIZ07 <itokianarabarivelo@gmail.com>
2026-06-12 07:23:46 +0300

    update link

--- src/main/java/com/mnemos/servlet/FrontControllerServlet.java
+++ src/main/java/com/mnemos/servlet/FrontControllerServlet.java
@@ -11,7 +11,7 @@ import java.io.PrintWriter;
 public class FrontControllerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String URL = req.getContextPath();
+        String URL = req.getRequestURI();
         processRequest(resp, URL);
     }
 
